652f8b6e0c28135141a9234272b3d337027e508d
urassh <shuto.1011@icloud.com>
2023-01-17 11:39:58 +0900

    問題を5問ごとに一周できるようにしたが、問題数を超えてしまった時の対処は未完了。

--- src/Components/Answer.jsx
+++ src/Components/Answer.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Modal from "react-modal";
 import styled from "styled-components";
 
 import '../App.css';
-import { QUESTIONS } from './questionData';
+import { QUESTIONS, QuizSetting } from './questionData';
 import correctImage from "../Images/correct.png";
 import incorrectImage from "../Images/incorrect.png"
 
 const Answer = ()=>{
+    const Max = QuizSetting.MIN + QuizSetting.quizLength;
+    const settingQuetions = QUESTIONS.slice(QuizSetting.MIN, Max);
     const history = useHistory();
-
     const [quiz, setQuiz] = useState({
-        index: 0,
+        index: QuizSetting.index,
         isCorrect: false,
         correctCount: 0,
         totalPoint: 0,
     });
-
     const [showResultModal, setshowResultModal] = useState(false);
+
+    useEffect(()=>{
+        const MAX = QuizSetting.MIN + QuizSetting.quizLength;
+        if(QUESTIONS.length < MAX ) {
+            QuizSetting.MIN = 0;
+        }
+    });
 
     const quizHandler = (e)=> {
         const answered = e.currentTarget.id;
@@ -28,24 +35,27 @@ const Answer = ()=>{
 
     const nextQuiz = () => {
         setshowResultModal(false);
-        if (quiz.index === QUESTIONS.length-1){
+        QuizSetting.index += 1;
+        if (quiz.index === settingQuetions.length-1){
+            QuizSetting.MIN = QuizSetting.index + QuizSetting.MIN;
+            QuizSetting.index = 0;
             history.push({
                 pathname: '/result',
                 state: {correct: quiz.correctCount, point: quiz.totalPoint}
             });
         } else {
-            setQuiz({...quiz, index: quiz.index+1, isCorrect: false});
+            setQuiz({...quiz, index: QuizSetting.index, isCorrect: false});
         }
     }
 
     const quizJudge = (answerButtonIndex) => {
-        const correct = QUESTIONS[quiz.index].correct;
+        const correct = settingQuetions[quiz.index].correct;
         const answer = Number(answerButtonIndex);
         if(answer === correct) {
             setQuiz({...quiz,
                 isCorrect: true,
                 correctCount: quiz.correctCount+1,
-                totalPoint: quiz.totalPoint +  QUESTIONS[quiz.index].point
+                totalPoint: quiz.totalPoint +  settingQuetions[quiz.index].point
             });
         } else {
             setQuiz({...quiz, isCorrect: false});
@@ -71,20 +81,20 @@ const Answer = ()=>{
             
             <h1>第 {quiz.index+1} 問</h1>
             <p id="quiz-text">
-                {QUESTIONS[quiz.index].text}
+                {settingQuetions[quiz.index].text}
             </p>
             <div className="answer-buttons">
                 <button id="1" className="answer-button button-1" onClick={quizHandler}>
-                    {QUESTIONS[quiz.index].buttonsList[0]}
+                    {settingQuetions[quiz.index].buttonsList[0]}
                 </button>
                 <button id="2" className="answer-button button-2" onClick={quizHandler}>
-                    {QUESTIONS[quiz.index].buttonsList[1]}
+                    {settingQuetions[quiz.index].buttonsList[1]}
                 </button>
                 <button id="3" className="answer-button button-3" onClick={quizHandler}>
-                    {QUESTIONS[quiz.index].buttonsList[2]}
+                    {settingQuetions[quiz.index].buttonsList[2]}
                 </button>
                 <button id="4" className="answer-button button-4" onClick={quizHandler}>
-                    {QUESTIONS[quiz.index].buttonsList[3]}
+                    {settingQuetions[quiz.index].buttonsList[3]}
                 </button> 
             </div>
         </div>
---
+++ src/Components/ErrorBoundary.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+    static getDerivedStateFromError(error) {
+        return {hasError : true};
+    }
+
+    componentDidCatch(error, errorInfo) {
+        
+    }
+}
+
+export {ErrorBoundary};
